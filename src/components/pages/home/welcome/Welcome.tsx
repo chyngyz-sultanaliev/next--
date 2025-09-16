651e824eae5f200b2df199417ab719a48ff33cd8
chyngyz-sultanaliev <sultanalievcyngyz98@gmail.com>
2025-09-16 14:48:12 +0600

    home

--- src/components/pages/home/welcome/Welcome.tsx
+++ src/components/pages/home/welcome/Welcome.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Product from "../../product/Product";
 
 const Welcome = () => {
-  return <div>WELcome</div>;
+  return (
+    <div className="container">
+      <center
+        style={{
+          fontSize: "24px",
+          color: "white",
+          padding: "20px 0",
+        }}
+      >
+        Home
+      </center>
+      <Product />
+    </div>
+  );
 };
 
 export default Welcome;
